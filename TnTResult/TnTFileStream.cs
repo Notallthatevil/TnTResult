d6f8dbd6c3a8cb06ec55ee85e862f231a0af2570
Nathan Tripp <ntripp@workfirstcasualty.com>
2024-09-16 13:41:45 -0600

    Made TnTFileStream Disposable

--- TnTResult/TnTFileStream.cs
+++ TnTResult/TnTFileStream.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Represents a file stream for TnTResult.
 /// </summary>
-public class TnTFileStream {
+public class TnTFileStream : IDisposable {
 
     /// <summary>
     /// Gets the content type of the file.
@@ -19,4 +19,8 @@ public class TnTFileStream {
     /// Gets the stream associated with the file.
     /// </summary>
     public Stream? Stream { get; init; }
+
+    public void Dispose() {
+        Stream?.Dispose();
+    }
 }
