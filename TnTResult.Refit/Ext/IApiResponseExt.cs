e14043ac5688683663d4df9156da827defe3670b
Nathan Tripp <ntripp@workfirstcasualty.com>
2024-12-13 13:28:00 -0700

    Refactor IApiResponseExt for clarity and functionality
    The code in `IApiResponseExt.cs` has been refactored to improve clarity and functionality. Key changes include the addition of new classes (`PromotingApiResponse` and `StreamApiResponseWrapper`) to encapsulate the behavior of `IApiResponse` and `IApiResponse<Stream>`, respectively. This encapsulation allows for cleaner handling of API responses and their conversion to `ITnTResult`. The method `ToTnTResult` has been simplified to use these new classes, reducing the complexity of the original implementation. Additionally, the documentation comments have been updated for clarity, and error handling has been enhanced to provide more informative failure messages. The use of `try` and `finally` blocks ensures that resources are properly disposed of, improving memory management.

--- TnTResult.Refit/Ext/IApiResponseExt.cs
+++ TnTResult.Refit/Ext/IApiResponseExt.cs
@@ -1,124 +1,123 @@
 ﻿using Refit;
 using System.Net;
+using System.Net.Http.Headers;
 
 namespace TnTResult.Refit.Ext;
 
+/// <summary>
+///     Extension methods for IApiResponse.
+/// </summary>
 public static class IApiResponseExt {
 
     /// <summary>
-    /// Converts an <see cref="IApiResponse" /> to an <see cref="ITnTResult" />.
+    ///     Converts an IApiResponse to an ITnTResult.
     /// </summary>
-    /// <param name="apiResponse">The <see cref="IApiResponse" /> to convert.</param>
-    /// <returns>An <see cref="ITnTResult" /> representing the conversion result.</returns>
-    public static ITnTResult ToTnTResult(this IApiResponse apiResponse) {
-        if (apiResponse == null) {
-            return ITnTResult.Failure("Failed with empty response from the server");
-        }
-        else {
-            if (!apiResponse.IsSuccessStatusCode) {
-                if (apiResponse.StatusCode == HttpStatusCode.InternalServerError) {
-                    return ITnTResult.Failure(apiResponse.Error.ReasonPhrase?.Trim('"') ?? $"Failed with status code {apiResponse.StatusCode} {apiResponse.ReasonPhrase}");
+    /// <param name="apiResponse">The API response.</param>
+    /// <returns>The result of the operation.</returns>
+    public static ITnTResult ToTnTResult(this IApiResponse apiResponse) => new PromotingApiResponse(apiResponse).ToTnTResult<object>();
+
+    /// <summary>
+    ///     Converts an IApiResponse of type TSuccess to an ITnTResult of type TSuccess.
+    /// </summary>
+    /// <typeparam name="TSuccess">The type of the success value.</typeparam>
+    /// <param name="apiResponse">The API response.</param>
+    /// <returns>The result of the operation.</returns>
+    public static ITnTResult<TSuccess> ToTnTResult<TSuccess>(this IApiResponse<TSuccess> apiResponse) {
+        try {
+            if (apiResponse is null) {
+                return ITnTResult<TSuccess>.Failure(new Exception("Failed with empty response from the server"));
+            }
+            else {
+                if (apiResponse.IsSuccessStatusCode) {
+                    if (apiResponse.StatusCode == HttpStatusCode.Created && apiResponse.Headers.Location is not null && typeof(TSuccess) == typeof(string)) {
+                        return ITnTResult<TSuccess>.Success((TSuccess)Convert.ChangeType(apiResponse.Headers.Location.ToString(), typeof(TSuccess)));
+                    }
+                    else {
+                        return ITnTResult<TSuccess>.Success(apiResponse.Content);
+                    }
                 }
                 else {
-                    return ITnTResult.Failure(apiResponse.Error.Content?.Trim('"') ?? $"Failed with status code {apiResponse.StatusCode} {apiResponse.ReasonPhrase}");
+                    return apiResponse.StatusCode == HttpStatusCode.InternalServerError
+                        ? ITnTResult<TSuccess>.Failure(apiResponse.Error.ReasonPhrase?.Trim('"') ?? $"Failed with status code {apiResponse.StatusCode} {apiResponse.ReasonPhrase}")
+                        : ITnTResult<TSuccess>.Failure(apiResponse.Error.Content?.Trim('"') ?? $"Failed with status code {apiResponse.StatusCode} {apiResponse.ReasonPhrase}");
                 }
             }
-
-            return ITnTResult.Successful;
+        }
+        finally {
+            apiResponse?.Dispose();
         }
     }
 
     /// <summary>
-    /// Converts an <see cref="IApiResponse{TSuccess}" /> to an <see cref="ITnTResult{TSuccess}" />.
+    ///     Converts an IApiResponse of type Stream to an ITnTResult of type TnTFileDownload.
     /// </summary>
-    /// <typeparam name="TSuccess">The type of the success content.</typeparam>
-    /// <param name="apiResponse">The <see cref="IApiResponse{TSuccess}" /> to convert.</param>
-    /// <returns>An <see cref="ITnTResult{TSuccess}" /> representing the conversion result.</returns>
-    public static ITnTResult<TSuccess> ToTnTResult<TSuccess>(this IApiResponse<TSuccess> apiResponse) {
-        if (apiResponse?.IsSuccessStatusCode == true) {
-            if (apiResponse.StatusCode == HttpStatusCode.Created && apiResponse.Headers.Location is not null && typeof(TSuccess) == typeof(string)) {
-                return ITnTResult<TSuccess>.Success((TSuccess)Convert.ChangeType(apiResponse.Headers.Location.ToString(), typeof(TSuccess)));
-            }
+    /// <param name="apiResponse">The API response.</param>
+    /// <returns>The result of the operation.</returns>
+    public static ITnTResult<TnTFileDownload> ToTnTResult(this IApiResponse<Stream> apiResponse) => new StreamApiResponseWrapper(apiResponse).ToTnTResult<TnTFileDownload>();
 
-            return ITnTResult<TSuccess>.Success(apiResponse.Content);
-        }
-        else {
-            var result = ((IApiResponse?)apiResponse)?.ToTnTResult();
-            return ITnTResult<TSuccess>.Failure(result?.Error ?? new Exception("An unknown error occurred"));
-        }
+    /// <summary>
+    ///     Asynchronously converts a Task of IApiResponse to an ITnTResult.
+    /// </summary>
+    /// <param name="task">The task representing the API response.</param>
+    /// <returns>A task representing the result of the operation.</returns>
+    public static async Task<ITnTResult> ToTnTResultAsync(this Task<IApiResponse> task) => await task.ContinueWith(t => t.Result.ToTnTResult());
+
+    /// <summary>
+    ///     Asynchronously converts a Task of IApiResponse of type TSuccess to an ITnTResult of type TSuccess.
+    /// </summary>
+    /// <typeparam name="TSuccess">The type of the success value.</typeparam>
+    /// <param name="task">The task representing the API response.</param>
+    /// <returns>A task representing the result of the operation.</returns>
+    public static async Task<ITnTResult<TSuccess>> ToTnTResultAsync<TSuccess>(this Task<IApiResponse<TSuccess>> task) => await task.ContinueWith(t => t.Result.ToTnTResult());
+
+    /// <summary>
+    ///     Asynchronously converts a Task of IApiResponse of type Stream to an ITnTResult of type TnTFileDownload.
+    /// </summary>
+    /// <param name="task">The task representing the API response.</param>
+    /// <returns>A task representing the result of the operation.</returns>
+    public static async Task<ITnTResult<TnTFileDownload>> ToTnTResultAsync(this Task<IApiResponse<Stream>> task) => await task.ContinueWith(t => t.Result.ToTnTResult());
+
+    /// <summary>
+    ///     Internal implementation of IApiResponse for object type.
+    /// </summary>
+    /// <param name="apiResponse">The original API response.</param>
+    private class PromotingApiResponse(IApiResponse apiResponse) : IApiResponse<object> {
+        public object? Content => null;
+        public HttpContentHeaders? ContentHeaders => apiResponse.ContentHeaders;
+        public ApiException? Error => apiResponse.Error;
+        public HttpResponseHeaders Headers => apiResponse.Headers;
+        public bool IsSuccessful => apiResponse.IsSuccessful;
+        public bool IsSuccessStatusCode => apiResponse.IsSuccessStatusCode;
+        public string? ReasonPhrase => apiResponse.ReasonPhrase;
+        public HttpRequestMessage? RequestMessage => apiResponse.RequestMessage;
+        public HttpStatusCode StatusCode => apiResponse.StatusCode;
+        public Version Version => apiResponse.Version;
+
+        public void Dispose() => apiResponse.Dispose();
     }
 
     /// <summary>
-    /// Converts an <see cref="IApiResponse{Stream}" /> to an <see cref="ITnTResult{TnTFileStream}" />.
+    ///     Internal implementation of IApiResponse for TnTFileDownload type.
     /// </summary>
-    /// <param name="apiResponse">The <see cref="IApiResponse{Stream}" /> to convert.</param>
-    /// <returns>An <see cref="ITnTResult{TnTFileStream}" /> representing the conversion result.</returns>
-    public static ITnTResult<TnTFileDownload> ToTnTResult(this IApiResponse<Stream> apiResponse) {
-        if (apiResponse is not null && apiResponse.IsSuccessStatusCode == true) {
-            return ITnTResult<TnTFileDownload>.Success(new TnTFileDownload {
-                Contents = apiResponse.Content!,
-                Filename = apiResponse.ContentHeaders.ContentDisposition?.FileNameStar ?? apiResponse.ContentHeaders.ContentDisposition?.FileName!,
-                ContentType = apiResponse.ContentHeaders.ContentType?.MediaType!
-            });
-        }
-        else {
-            var result = ((IApiResponse?)apiResponse)?.ToTnTResult();
-            return ITnTResult<TnTFileDownload>.Failure(result?.Error ?? new Exception("An unknown error occurred"));
-        }
+    /// <param name="apiResponse">The original API response.</param>
+    private class StreamApiResponseWrapper(IApiResponse<Stream> apiResponse) : IApiResponse<TnTFileDownload> {
+
+        public TnTFileDownload? Content { get; } = new() {
+            Contents = apiResponse.Content!,
+            Filename = apiResponse.ContentHeaders!.ContentDisposition?.FileNameStar ?? apiResponse.ContentHeaders.ContentDisposition?.FileName!,
+            ContentType = apiResponse.ContentHeaders.ContentType?.MediaType!
+        };
+
+        public HttpContentHeaders? ContentHeaders => apiResponse.ContentHeaders;
+        public ApiException? Error => apiResponse.Error;
+        public HttpResponseHeaders Headers => apiResponse.Headers;
+        public bool IsSuccessful => apiResponse.IsSuccessful;
+        public bool IsSuccessStatusCode => apiResponse.IsSuccessStatusCode;
+        public string? ReasonPhrase => apiResponse.ReasonPhrase;
+        public HttpRequestMessage? RequestMessage => apiResponse.RequestMessage;
+        public HttpStatusCode StatusCode => apiResponse.StatusCode;
+        public Version Version => apiResponse.Version;
+
+        public void Dispose() => apiResponse.Dispose();
     }
-
-    /// <summary>
-    /// Asynchronously converts a <see cref="Task{IApiResponse}" /> to an <see cref="ITnTResult" />.
-    /// </summary>
-    /// <param name="task">The <see cref="Task{IApiResponse}" /> to convert.</param>
-    /// <returns>
-    /// A task representing the asynchronous operation that returns an <see cref="ITnTResult" />.
-    /// </returns>
-    public static async Task<ITnTResult> ToTnTResultAsync(this Task<IApiResponse> task) => (await task).ToTnTResult();
-
-    /// <summary>
-    /// Asynchronously converts a <see cref="Task{IApiResponse{TSuccess}}" /> to an <see
-    /// cref="ITnTResult{TSuccess}" />.
-    /// </summary>
-    /// <typeparam name="TSuccess">The type of the success content.</typeparam>
-    /// <param name="task">The <see cref="Task{IApiResponse{TSuccess}}" /> to convert.</param>
-    /// <returns>
-    /// A task representing the asynchronous operation that returns an <see
-    /// cref="ITnTResult{TSuccess}" />.
-    /// </returns>
-    public static async Task<ITnTResult<TSuccess>> ToTnTResultAsync<TSuccess>(this Task<IApiResponse<TSuccess>> task) => (await task).ToTnTResult();
-
-    /// <summary>
-    /// Asynchronously converts a <see cref="Task{IApiResponse{Stream}}" /> to an <see
-    /// cref="ITnTResult{TnTFileStream}" />.
-    /// </summary>
-    /// <param name="task">The <see cref="Task{IApiResponse{Stream}}" /> to convert.</param>
-    /// <returns>
-    /// A task representing the asynchronous operation that returns an <see
-    /// cref="ITnTResult{TnTFileStream}" />.
-    /// </returns>
-    public static async Task<ITnTResult<TnTFileDownload>> ToTnTResultAsync(this Task<IApiResponse<Stream>> task) => (await task).ToTnTResult();
-
-    /// <summary>
-    /// Asynchronously converts a <see cref="Task{ApiResponse{TSuccess}}" /> to an <see
-    /// cref="ITnTResult{TSuccess}" />.
-    /// </summary>
-    /// <typeparam name="TSuccess">The type of the success content.</typeparam>
-    /// <param name="task">The <see cref="Task{ApiResponse{TSuccess}}" /> to convert.</param>
-    /// <returns>
-    /// A task representing the asynchronous operation that returns an <see
-    /// cref="ITnTResult{TSuccess}" />.
-    /// </returns>
-    public static async Task<ITnTResult<TSuccess>> ToTnTResultAsync<TSuccess>(this Task<ApiResponse<TSuccess>> task) => (await task).ToTnTResult();
-
-    /// <summary>
-    /// Asynchronously converts a <see cref="Task{ApiResponse{Stream}}" /> to an <see
-    /// cref="ITnTResult{TnTFileStream}" />.
-    /// </summary>
-    /// <param name="task">The <see cref="Task{ApiResponse{Stream}}" /> to convert.</param>
-    /// <returns>
-    /// A task representing the asynchronous operation that returns an <see
-    /// cref="ITnTResult{TnTFileStream}" />.
-    /// </returns>
-    public static async Task<ITnTResult<TnTFileDownload>> ToTnTResultAsync(this Task<ApiResponse<Stream>> task) => (await task).ToTnTResult();
 }
