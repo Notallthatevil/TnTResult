f663d2ffcffaf794d95628d2b63e5a32b46bede4
Nate Tripp <nate.tripp13448@gmail.com>
2025-06-03 12:08:06 -0600

    Enhance exception classes with documentation and constructors
    - Updated `ForbiddenException` with XML documentation and a parameterless constructor.
    - Refined `NotFoundException` with XML documentation and a new constructor for entity type and key.

--- TnTResult/Exceptions/ForbiddenException.cs
+++ TnTResult/Exceptions/ForbiddenException.cs
@@ -6,4 +6,22 @@ using System.Threading.Tasks;
 
 namespace TnTResult.Exceptions;
 
-public class ForbiddenException() : Exception();
+/// <summary>
+///     Represents an exception that is thrown when a forbidden action is attempted.
+/// </summary>
+public class ForbiddenException : Exception {
+
+    /// <inheritdoc />
+    public ForbiddenException(string? message) : base(message) {
+    }
+
+    /// <inheritdoc />
+    public ForbiddenException(string? message, Exception? innerException) : base(message, innerException) {
+    }
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="ForbiddenException" /> class.
+    /// </summary>
+    public ForbiddenException() {
+    }
+}
--- TnTResult/Exceptions/NotFoundException.cs
+++ TnTResult/Exceptions/NotFoundException.cs
@@ -5,17 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TnTResult.Exceptions;
-public class NotFoundException : System.Exception {
-    public NotFoundException() : base() {
+
+/// <summary>
+///     Represents an exception that is thrown when a requested entity is not found.
+/// </summary>
+public class NotFoundException : Exception {
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="NotFoundException" /> class.
+    /// </summary>
+    public NotFoundException() {
     }
 
+    /// <inheritdoc />
     public NotFoundException(string? message) : base(message) {
     }
 
-    public NotFoundException(string? message, System.Exception? innerException) : base(message, innerException) {
+    /// <inheritdoc />
+    public NotFoundException(string? message, Exception? innerException) : base(message, innerException) {
     }
 
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="NotFoundException" /> class with the specified entity type and key.
+    /// </summary>
+    /// <param name="entityType">The type of the entity that was not found.</param>
+    /// <param name="key">       The key of the entity that was not found.</param>
     public NotFoundException(Type entityType, object key) : base($"Failed to find {entityType.Name} with primary key {key}") {
     }
 }
-
